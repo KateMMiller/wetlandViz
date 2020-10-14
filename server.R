1bffc3ca6cfd7c941a07b22a9198b0d267a5d5f2
KateMMiller <kathryn_miller@nps.gov>
2020-10-14 15:47:51 -0400

    updated leaflet tiles

--- server.R
+++ server.R
@@ -26,22 +26,22 @@ server <- function(input, output) {
     
   })
   
-  # NPS tiles
-  # NPSbasic<-"https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck58pyquo009v01p99xebegr9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
-  # 
-  # NPSimagery="https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck72fwp2642dv07o7tbqinvz4/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
-  # 
-  # NPSslate="https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck5cpvc2e0avf01p9zaw4co8o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
-  # 
-  # NPSlight="https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck5cpia2u0auf01p9vbugvcpv/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
-  # 
-  # # Make NPS map Attribution
-  # NPSAttrib <- HTML("<a href='https://www.nps.gov/npmap/disclaimer/'>Disclaimer</a> | 
-  #     &copy; <a href='http://openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a> contributors |
-  #     <a class='improve-park-tiles' 
-  #     href='http://insidemaps.nps.gov/places/editor/#background=mapbox-satellite&map=4/-95.97656/39.02772&overlays=park-tiles-overlay'
-  #     target='_blank'>Improve Park Tiles</a>")
+  NPSbasic = "https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck58pyquo009v01p99xebegr9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
   
+  NPSimagery = "https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck72fwp2642dv07o7tbqinvz4/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
+  
+  NPSslate = "https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck5cpvc2e0avf01p9zaw4co8o/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
+  
+  NPSlight = "https://atlas-stg.geoplatform.gov/styles/v1/atlas-user/ck5cpia2u0auf01p9vbugvcpv/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYXRsYXMtdXNlciIsImEiOiJjazFmdGx2bjQwMDAwMG5wZmYwbmJwbmE2In0.lWXK2UexpXuyVitesLdwUg"
+  
+   
+  # Make NPS map Attribution
+  NPSAttrib<-HTML("<a href='https://www.nps.gov/npmap/disclaimer/'>Disclaimer</a> | 
+      &copy; <a href='http://openstreetmap.org/copyright' target='_blank'>OpenStreetMap</a> contributors |
+      <a class='improve-park-tiles' 
+      href='http://insidemaps.nps.gov/places/editor/#background=mapbox-satellite&map=4/-95.97656/39.02772&overlays=park-tiles-overlay'
+      target='_blank'>Improve Park Tiles</a>")
+
   # Render wetland map
   output$WetlandMap <- renderLeaflet({
     leaflet() %>%
@@ -58,36 +58,32 @@ server <- function(input, output) {
         lat1 = 44.9,
         lat2 = 43.5
       ) %>% 
-      addTiles(group = "OpenStreetMap") #%>% 
-      # addTiles(
-      #   group = "Map",
-      #   urlTemplate = NPSbasic,
-      #   attribution = NPSAttrib,
-      #   options = tileOptions(minZoom = 8)
-      # ) %>%
-      # addTiles(
-      #   group = "Imagery",
-      #   urlTemplate = NPSimagery,
-      #   attribution = NPSAttrib,
-      #   options = tileOptions(minZoom = 8)
-      # ) %>%
-      # addTiles(
-      #   group = "Light",
-      #   urlTemplate = NPSlight,
-      #   attribution = NPSAttrib,
-      #   options = tileOptions(minZoom = 8)
-      # ) %>%
-      # addTiles(
-      #   group = "Slate",
-      #   urlTemplate = NPSslate,
-      #   attribution = NPSAttrib,
-      #   options = tileOptions(minZoom = 8)
-      # ) %>%
-      # addLayersControl(
-      #   map = ., 
-      #   baseGroups = c("OpenStreet","Map", "Imagery", "Light", "Slate"),
-      #   options = layersControlOptions(collapsed = T)
-      # )
+      #addTiles(group = "OpenStreetMap") #%>% 
+      addTiles(
+        group = "Map",
+        urlTemplate = NPSbasic,
+        attribution = NPSAttrib) %>%
+      addTiles(
+        group = "Imagery",
+        urlTemplate = NPSimagery,
+        attribution = NPSAttrib) %>%
+      addTiles(
+        group = "Light",
+        urlTemplate = NPSlight,
+        attribution = NPSAttrib,
+        options = tileOptions(minZoom = 8)
+      ) %>%
+      addTiles(
+        group = "Slate",
+        urlTemplate = NPSslate,
+        attribution = NPSAttrib,
+        options = tileOptions(minZoom = 8)
+      ) %>%
+      addLayersControl(
+        map = .,
+        baseGroups = c("Map", "Imagery", "Light", "Slate"),
+        options = layersControlOptions(collapsed = T)
+      )
   })
 
   
